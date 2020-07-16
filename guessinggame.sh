913a1af8a1eda95396fbbba647924223193affa6
Shravani GN <32588934+shravanign@users.noreply.github.com>
2020-07-16 16:26:07 +0530

    Add files via upload

---
+++ guessinggame.sh
@@ -0,0 +1,21 @@
+count=$(ls -l | wc -l)
+
+function check {
+  if [[ $1 -lt $2 ]]
+  then
+      echo "It's lower!"
+  elif [[ $1 -gt $2 ]]
+  then
+      echo "It's higher!"
+  else
+      echo "Congratulations! You have guessed it right"
+  fi
+}
+
+while [[ $count -ne $guess ]]
+do
+   read -p "Guess how many files are there in current repository? " guess
+   echo $(check  $guess $count)
+done
+
+
